afb73184c1d46ad7f43d113f0b413ef1c52088c7
oleina <oleina@google.com>
2024-12-14 10:22:26 -0800

    some tests for unionfind

--- days/sanity.test.cpp
+++ days/sanity.test.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 
 #include "../lib/test_util.hpp"
+#include "../lib/union_find.hpp"
+#include "../lib/grid.hpp"
 
 TEST(StringToVecInt, GivesValidOutput) {
  const auto out = string_to_vec_int("2997");
@@ -9,6 +11,23 @@ TEST(StringToVecInt, GivesValidOutput) {
  ASSERT_EQ(out[1], 9);
  ASSERT_EQ(out[2], 9);
  ASSERT_EQ(out[3], 7);
+}
+
+TEST(UnionFind, Works) {
+  union_find<Coord> uf({5, 5});
+
+  
+  ASSERT_EQ(uf.find({3, 3}), uf.find({3, 3})) << "A should be its own parent";
+
+  uf.join({3, 3}, {3, 2});
+  ASSERT_EQ(uf.find({3, 3}), uf.find({3, 2})) << "after joining parents should be the same";
+
+  uf.join({3, 3}, {3, 1});
+  ASSERT_EQ(uf.find({3, 2}), uf.find({3, 1})) << "siblings should have same parent";
+
+  ASSERT_NE(uf.find({3, 4}), uf.find({3, 1})) << "unrelated should have different same parent";
+
+  ASSERT_EQ(uf.all_parents().size(), (6 * 6) - 2) << "num parents should be equal to num groups";
 }
 
 #ifdef TEST_MAIN
